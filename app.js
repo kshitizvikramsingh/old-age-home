8b96d8238bd7e14c0324ca865797a23238c7520a
kshitiz vikram singh <kshitizvikramsingh123@gmail.com>
2023-12-13 10:22:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const express=require("express")
+const path=require("path")
+const app=express()
+
+const pubDir=path.join(__dirname,"/public")
+const viewsDir=path.join(__dirname,"/views")
+
+app.set("view engine","ejs")
+app.set("views",viewsDir)
+
+app.use(express.static(pubDir))
+
+app.get("/",(req,res)=>{
+    res.render("homepage.ejs")
+})
+app.get("/about",(req,res)=>{
+    res.render("about.ejs")
+})
+
+
+
+app.listen("80",()=>{
+    console.log("app is up on port 80")
+})
